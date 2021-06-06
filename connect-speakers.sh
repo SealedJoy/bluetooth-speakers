d3923e3d39e26e771f226a7c806104e84a0728b9
SealedJoy <sealedjoy@gmail.com>
2021-06-06 16:37:45 +0100

    update timeout

---
+++ connect-speakers.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo -e 'power on\n'
+sleep 3
+#echo -e 'pairable on\n'
+#sleep 2
+echo -e "connect $1\n"
+sleep 5
+echo -e 'quit\n'
---
+++ disconnect-speakers.sh
@@ -0,0 +1,6 @@
+#!/usr/bin//env bash
+echo -e "disconnect $1\n"
+sleep 1
+echo -e 'power off\n'
+sleep 0.5
+echo -e 'quit\n'
